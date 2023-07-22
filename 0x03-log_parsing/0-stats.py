1c1f40eeae8865642aad13cc7459bb0595be6fab
OmarKhalil10 <omar.khalil498@gmail.com>
2023-07-22 00:32:36 +0000

    fix syntax error in status.py script

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -33,7 +33,7 @@ dict_sc = {"200": 0,
 
 try:
     for line in sys.stdin:
-        parsed_line = line.split()  # ✄ trimming
+        parsed_line = line.split()  # trimming
         parsed_line = parsed_line[::-1]  # inverting
 
         if len(parsed_line) > 2:
